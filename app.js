cdf260e8451929f61d099d1940bc69eccc035d08
sanika <sanikaalmeida@gamil.com>
2024-03-11 15:30:22 +0530

    cors added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express=require('express')
 const {dbconnect}=require('./config/db')
+const cors=require('cors')
 require('dotenv').config()
 
 const port=process.env.port
 
 const app=express()
+app.use(cors())
 app.use(express.json())
 dbconnect()
 
